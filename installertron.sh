b5627957b208e2f71e76d5878b642bfe0e70d248
adnanbezerra <adnanbezerra@proton.me>
2023-11-27 20:43:13 -0300

    add zenity basics

--- installertron.sh
+++ installertron.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-VARIABLE=$(zenity --entry --title="Digite o nome da variável" --text="Bora movei major zé jorge digita aí")
+options=$(zenity --list --checklist --title="Installertron 3000" \
+  --text="Select what you want to install" \
+  --column="Option" \
+  --column="What will you install?" \
+  "1" \
+  "NodeJS" \
+  "3" \
+  "PostgreSQL");
 
-echo $VARIABLE
+IFS='|' read -r -a array <<< "$options"
+
+for element in ${array[@]}
+do
+  echo $element
+done
 
